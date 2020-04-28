71aea26992ecfb97007b406a307898ca573cd476
sakanaman <sakana1231@icloud.com>
2020-04-28 17:00:39 +0900

    add distance sampling

--- main.cpp
+++ main.cpp
@@ -75,42 +75,7 @@ inline bool intersect(const Ray &r, double &t, int &id)
         }
     return t < inf;
 }
-Vec radiance(const Ray &r, int depth, unsigned short *Xi)
-{
-    double t;   // distance to intersection
-    int id = 0; // id of intersected object
-    if (!intersect(r, t, id))
-        return Vec();                // if miss, return black
-    const Sphere &obj = spheres[id]; // the hit object
-    Vec x = r.o + r.d * t, n = (x - obj.p).norm(), nl = n.dot(r.d) < 0 ? n : n * -1, f = obj.c;
-    double p = f.x > f.y && f.x > f.z ? f.x : f.y > f.z ? f.y : f.z; // max refl
-    if (++depth > 5)
-        if (erand48(Xi) < p)
-            f = f * (1 / p);
-        else
-            return obj.e; //R.R.
-    if (obj.refl == DIFF)
-    { // Ideal DIFFUSE reflection
-        double r1 = 2 * M_PI * erand48(Xi), r2 = erand48(Xi), r2s = sqrt(r2);
-        Vec w = nl, u = ((fabs(w.x) > .1 ? Vec(0, 1) : Vec(1)) % w).norm(), v = w % u;
-        Vec d = (u * cos(r1) * r2s + v * sin(r1) * r2s + w * sqrt(1 - r2)).norm();
-        return obj.e + f.mult(radiance(Ray(x, d), depth, Xi));
-    }
-    else if (obj.refl == SPEC) // Ideal SPECULAR reflection
-        return obj.e + f.mult(radiance(Ray(x, r.d - n * 2 * n.dot(r.d)), depth, Xi));
-    Ray reflRay(x, r.d - n * 2 * n.dot(r.d)); // Ideal dielectric REFRACTION
-    bool into = n.dot(nl) > 0;                // Ray from outside going in?
-    double nc = 1, nt = 1.5, nnt = into ? nc / nt : nt / nc, ddn = r.d.dot(nl), cos2t;
-    if ((cos2t = 1 - nnt * nnt * (1 - ddn * ddn)) < 0) // Total internal reflection
-        return obj.e + f.mult(radiance(reflRay, depth, Xi));
-    Vec tdir = (r.d * nnt - n * ((into ? 1 : -1) * (ddn * nnt + sqrt(cos2t)))).norm();
-    double a = nt - nc, b = nt + nc, R0 = a * a / (b * b), c = 1 - (into ? -ddn : tdir.dot(n));
-    double Re = R0 + (1 - R0) * c * c * c * c * c, Tr = 1 - Re, P = .25 + .5 * Re, RP = Re / P, TP = Tr / (1 - P);
-    return obj.e + f.mult(depth > 2 ? (erand48(Xi) < P ? // Russian roulette
-                                           radiance(reflRay, depth, Xi) * RP
-                                                       : radiance(Ray(x, tdir), depth, Xi) * TP)
-                                    : radiance(reflRay, depth, Xi) * Re + radiance(Ray(x, tdir), depth, Xi) * Tr);
-}
+
 
 //param
 constexpr double kEPS = 1e-6;
@@ -165,7 +130,71 @@ double distance_sample(const Ray& ray, const Vec& light_pos, const double u,
 
 Vec radiance_density(const Ray &r, int depth, unsigned short *Xi)
 {
-    
+    double t;   // distance to intersection
+    int id = 0; // id of intersected object
+
+    Vec contribute = Vec(0.0, 0.0, 0.0);
+
+    double tNear, tFar;
+    if (!intersect(r, t, id)) //何にも当たらなかった
+    {
+        tNear = 0.0;
+        tFar = Max_dist;
+    }
+    else
+    {
+        tNear = 0.0;
+        tFar = t;
+        // printf("in"); -->入ってる
+
+        //点光源と衝突点でNEE
+        const Sphere &obj = spheres[id];
+        Vec hitPos = r.o + r.d * t;
+        Vec h2l = (point_light-hitPos).norm();
+        Vec n = (hitPos - obj.p).norm();
+        double l = (point_light - hitPos).length();
+        double costerm = h2l.dot(n);
+
+        double shadow_t;
+        int shadow_id = 0;
+        Ray shadow_r(hitPos, h2l);
+        bool is_shadowhit = intersect(shadow_r, shadow_t, shadow_id);
+        if(is_shadowhit == false)
+        {
+            contribute = contribute + (Albedo * (1.0/M_PI)).mult(light_power * costerm * (1.0/(l*l))) * exp(-sigma_t*(l + t));
+        }
+        // contribute = Vec(1.0, 0, 0);
+        // return contribute;-->ちゃんと衝突してる。
+    }
+
+    /// Equi-Angular Sampleの開始だ!!
+
+    double pdf, scatter_dist;
+    scatter_dist = distance_sample(r, point_light, erand48(Xi), 
+                                    tNear, tFar, &pdf);
+    //printf("%f\n",pdf);
+
+    Vec scatter_point = r.o + r.d * scatter_dist;
+    Vec scatter_dir = (point_light - scatter_point).norm();
+
+    double shadow_t;
+    int shadow_id = 0;
+    Ray shadow_r(scatter_point, scatter_dir);
+    bool is_shadowhit = intersect(shadow_r, shadow_t, shadow_id);
+    if(!is_shadowhit || (is_shadowhit && shadow_t > (point_light - scatter_point).length()))
+    {
+        // printf("in"); --> 入ってる
+        double l2s = (point_light - scatter_point).length();
+        //printf("%f\n", l2s);
+
+        Vec plus = light_power * (1.0/(l2s * l2s)) * sigma_s * exp(-sigma_t*(scatter_dist + l2s)) * (1.0/pdf);
+        //printf("%f\n", -sigma_t*(scatter_dist + l2s));
+        //printf("(%f, %f, %f)\n", plus.x, plus.y, plus.z);
+
+        contribute = contribute + light_power * (1.0/(l2s * l2s)) * sigma_s * exp(-sigma_t*(scatter_dist + l2s)) * (1.0/pdf);
+    }
+    //printf("(%f, %f, %f)\n",contribute.x,contribute.y,contribute.z);
+    return contribute;
 }
 
 Vec radiance_equiangular(const Ray &r, int depth, unsigned short *Xi)
@@ -256,7 +285,7 @@ int main(int argc, char *argv[])
                         double r2 = 2 * erand48(Xi), dy = r2 < 1 ? sqrt(r2) - 1 : 1 - sqrt(2 - r2);
                         Vec d = cx * (((sx + .5 + dx) / 2 + x) / w - .5) +
                                 cy * (((sy + .5 + dy) / 2 + y) / h - .5) + cam.d;
-                        r = r + radiance_equiangular(Ray(cam.o + d * 140, d.norm()), 0, Xi) * (1. / samps);
+                        r = r + radiance_density(Ray(cam.o + d * 140, d.norm()), 0, Xi) * (1. / samps);
                     } // Camera rays are pushed ^^^^^ forward to start in interior
                     c[i] = c[i] + Vec(clamp(r.x), clamp(r.y), clamp(r.z)) * .25;
                 }
